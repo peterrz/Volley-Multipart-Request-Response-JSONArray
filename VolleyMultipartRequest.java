6614170141bbf8a01a5e50f062cbfdecd0e8320b
pourya Rezaee zadeh <peter.rezaeezadeh@gmail.com>
2019-07-20 20:37:11 +0430

    Add files via upload

---
+++ VolleyMultipartRequest.java
@@ -0,0 +1,246 @@
+package com.miradal.admin_recycling.Helper;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.jar.JarEntry;
+
+/**
+ * Created by Belal on 10/24/2017.
+ * Customise by pourya on 2019.
+ * add "UTF-8" encoder for persian words
+ * change response to JSONArray
+ */
+
+public class VolleyMultipartRequest extends Request<JSONArray> {
+
+    private final String twoHyphens = "--";
+    private final String lineEnd = "\r\n";
+    private final String boundary = "apiclient-" + System.currentTimeMillis();
+
+    private Response.Listener<JSONArray> mListener;
+    private Response.ErrorListener mErrorListener;
+    private Map<String, String> mHeaders;
+
+
+    public VolleyMultipartRequest(int method, String url,
+                                  Response.Listener<JSONArray> listener,
+                                  Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.mListener = listener;
+        this.mErrorListener = errorListener;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return (mHeaders != null) ? mHeaders : super.getHeaders();
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return "multipart/form-data;boundary=" + boundary;
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        try {
+            // populate text payload
+            Map<String, String> params = getParams();
+            if (params != null && params.size() > 0) {
+                textParse(dos, params, getParamsEncoding());
+
+
+            }
+
+            // populate data byte payload
+            Map<String, DataPart> data = getByteData();
+            if (data != null && data.size() > 0) {
+                dataParse(dos, data);
+            }
+
+            // close multipart form data after text and file data
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Custom method handle data payload.
+     *
+     * @return Map data part label with data byte
+     * @throws AuthFailureError
+     */
+    protected Map<String, DataPart> getByteData() throws AuthFailureError {
+        return null;
+    }
+
+    @Override
+    protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            return Response.success(new JSONArray(jsonString),d
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(JSONArray response) {
+        mListener.onResponse(response);
+    }
+
+    @Override
+    public void deliverError(VolleyError error) {
+        mErrorListener.onErrorResponse(error);
+    }
+
+    /**
+     * Parse string map into data output stream by key and value.
+     *
+     * @param dataOutputStream data output stream handle string parsing
+     * @param params           string inputs collection
+     * @param encoding         encode the inputs, default UTF-8
+     * @throws IOException
+     */
+    private void textParse(DataOutputStream dataOutputStream, Map<String, String> params, String encoding) throws IOException {
+        try {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+
+                buildTextPart(dataOutputStream, entry.getKey(),entry.getValue());
+
+
+            }
+        } catch (UnsupportedEncodingException uee) {
+            throw new RuntimeException("Encoding not supported: " + encoding, uee);
+        }
+    }
+
+    /**
+     * Parse data into data output stream.
+     *
+     * @param dataOutputStream data output stream handle file attachment
+     * @param data             loop through data
+     * @throws IOException
+     */
+    private void dataParse(DataOutputStream dataOutputStream, Map<String, DataPart> data) throws IOException {
+        for (Map.Entry<String, DataPart> entry : data.entrySet()) {
+            buildDataPart(dataOutputStream, entry.getValue(), entry.getKey());
+        }
+    }
+
+    /**
+     * Write string data into header and data output stream.
+     *
+     * @param dataOutputStream data output stream handle string parsing
+     * @param parameterName    name of input
+     * @param parameterValue   value of input
+     * @throws IOException
+     */
+    private void buildTextPart(DataOutputStream dataOutputStream, String parameterName, String parameterValue) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + parameterName + "\"" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+        dataOutputStream.writeBytes(URLEncoder.encode(parameterValue,"UTF-8") + lineEnd);
+
+
+    }
+
+    /**
+     * Write data file into header and data output stream.
+     *
+     * @param dataOutputStream data output stream handle data parsing
+     * @param dataFile         data byte as DataPart from collection
+     * @param inputName        name of data input
+     * @throws IOException
+     */
+    private void buildDataPart(DataOutputStream dataOutputStream, DataPart dataFile, String inputName) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" +
+                inputName + "\"; filename=\"" + dataFile.getFileName() + "\"" + lineEnd);
+        if (dataFile.getType() != null && !dataFile.getType().trim().isEmpty()) {
+            dataOutputStream.writeBytes("Content-Type: " + dataFile.getType() + lineEnd);
+        }
+        dataOutputStream.writeBytes(lineEnd);
+
+        ByteArrayInputStream fileInputStream = new ByteArrayInputStream(dataFile.getContent());
+        int bytesAvailable = fileInputStream.available();
+
+        int maxBufferSize = 1024 * 1024;
+        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        byte[] buffer = new byte[bufferSize];
+
+        int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            dataOutputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        dataOutputStream.writeBytes(lineEnd);
+    }
+
+  public static class DataPart {
+        private String fileName;
+        private byte[] content;
+        private String type;
+
+        public DataPart() {
+        }
+
+        public DataPart(String name, byte[] data) {
+            fileName = name;
+            content = data;
+        }
+
+        String getFileName() {
+            return fileName;
+        }
+
+        byte[] getContent() {
+            return content;
+        }
+
+        String getType() {
+            return type;
+        }
+
+    }
+}
